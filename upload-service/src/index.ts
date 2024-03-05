76ee705e1e2d4e73379bf57d113d257d7a76afe8
Alex Gushulak <alex.gushulak@gmail.com>
2024-03-05 13:19:27 -0500

    Push uuid-v4 ID to redis queue

--- upload-service/src/index.ts
+++ upload-service/src/index.ts
@@ -1,20 +1,48 @@
-import express from "express"
+import express from "express"   
 import cors from "cors"
+import { createClient } from "redis"
+import { v4 as uuidv4 } from "uuid"
+
+
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+const client = createClient({
+    url: "redis://default:Nph3H6dHH5JJae1D1B24CL5M6abmjLdp@monorail.proxy.rlwy.net:24898"
+})
+
+client.connect()
+
+client.on("error", (error) => {
+    console.error(`Redis Error: ${error}`)
+})
+
+client.on("connect", () => {
+    console.info("Connected to Redis")
+})
+
+
+
 
 app.post("/deploy", async (req, res) => {
     const repoUrl: string = req.body.repoUrl
-    const id: string = '12341234'
-    console.log(repoUrl)
-    res.send({
-        id: id,
-        status: 'bhai'
+    const id: string = uuidv4()
+    // upload files locally
+    // push files to s3
+    // delete local files
+    // profit
+    client.lPush("deploy-queue", id).then(() => {
+        console.log(`Upload request for ${repoUrl} with id ${id} queued`)
+    }).then(() => {
+        res.send({
+            id: id,
+            repoUrl: repoUrl,
+            status: 'uploaded'
+        })
     })
-});
+})
 
 app.get("/status", async (req) => {
     const id: string = req.query['id'] as string
